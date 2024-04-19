e5221297560bfb1ac99a9b474235b0ffa38123a6
peelromcok <jajcokgtgto@gmail.com>
2024-04-19 13:49:41 +0200

    x

--- client/api_client.py
+++ client/api_client.py
@@ -1,7 +1,13 @@
 import requests
-from .config import API_URL, API_KEY
+from config import API_URL, API_KEY
+# API_URL = "http://api.airvisual.com/v2/"
+# API_KEY = "18b34ba2-c1fb-4892-b559-1b4e492ef7b6"
 
 class AirQualityClient:
     def get_air_quality_data(self, city):
-        response = requests.get(f"{API_URL}/v2/city?city={city}&key={API_KEY}")
+        response = requests.get(f"{API_URL}/city?city={city}&key={API_KEY}")
+        # response = requests.get("http://api.airvisual.com/v2/city?city=Warszawa&key=18b34ba2-c1fb-4892-b559-1b4e492ef7b6")
+     #   curl --location -g 'http://api.airvisual.com/v2/nearest_station?key={{YOUR_API_KEY}}'
+        
+
         return response.json()
---
+++ client/config.py
@@ -0,0 +1,2 @@
+API_URL = "http://api.airvisual.com/v2/"
+API_KEY = "18b34ba2-c1fb-4892-b559-1b4e492ef7b6"
